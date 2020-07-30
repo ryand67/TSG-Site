e784265d2ade8cd421c65de5cca3dcd6a96a8ac4
ryand67 <rwdevenney@gmail.com>
2020-07-30 18:27:58 -0400

    media queries

--- client/src/components/OurTeam/TeamCard/TeamCard.js
+++ client/src/components/OurTeam/TeamCard/TeamCard.js
@@ -9,7 +9,7 @@ const TeamCard = (props) => {
             <h4 className="teamCardName">{props.name}</h4>
             <p className="teamCardDesc">{props.desc}</p>
             <div className="teamCardContact">
-                <p className="teamCardEmail"><i class="fas fa-envelope"></i>{props.email}</p>
+                <p className="teamCardEmail"><a href={`mailto:${props.email}`}><i class="fas fa-envelope"></i>{props.email}</a></p>
                 <p className="teamCardPhone"><i class="fas fa-phone-alt"></i>{props.phone}</p>
             </div>
         </div>
