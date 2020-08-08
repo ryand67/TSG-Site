004e0861bab221d2bef4d5c1260a12504ece5ee7
ryand67 <rwdevenney@gmail.com>
2020-08-07 21:32:09 -0400

    albums load

--- client/src/components/AdultsOnly/AdultsOnly.js
+++ client/src/components/AdultsOnly/AdultsOnly.js
@@ -5,20 +5,24 @@ const AdultsOnly = () => {
 
     const [currentCountry, setCountry] = useState('Riviera Maya/Cancun');
 
-    const cancunLinks=['https://www.facebook.com/travelsalesgroup/photos/?tab=album&album_id=540565556097725', 'https://www.facebook.com/travelsalesgroup/photos/?tab=album&album_id=728746207279658']
+    const cancunLinks=['https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmedia%2Fset%2F%3Fvanity%3Dtravelsalesgroup%26set%3Da.540565556097725&show_text=true&width=552&height=792&appId', 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmedia%2Fset%2F%3Fvanity%3Dtravelsalesgroup%26set%3Da.728746207279658&show_text=true&width=552&height=792&appId', 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmedia%2Fset%2F%3Fvanity%3Dtravelsalesgroup%26set%3Da.540622576092023&show_text=true&width=552&height=792&appId', 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmedia%2Fset%2F%3Fvanity%3Dtravelsalesgroup%26set%3Da.1029168537237422&show_text=true&width=552&height=880&appId', 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmedia%2Fset%2F%3Fvanity%3Dtravelsalesgroup%26set%3Da.1547714015382869&show_text=true&width=552&height=792&appId']
 
     const iframeStyle = {
         border: 'none',
-        overflow: 'hidden',
-        width: '552',
-        height: '792'
+        overflow: 'hidden'
     }
 
     const loadAlbums = (arr) => {
         let result = arr.map(item => {
-            return <p>{item}</p>
+            return <iframe src={item} width="552" height="792" style={iframeStyle} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
         })
         return result;
+    }
+
+    const handleArrayPassing = () => {
+        if(currentCountry === 'Riviera Maya/Cancun') {
+            return cancunLinks;
+        }
     }
 
     const handleSelectChange = (e) => {
@@ -41,8 +45,9 @@ const AdultsOnly = () => {
                 <option value="Aruba">Aruba</option>
                 <option value="Costa Rica">Costa Rica</option>
             </select>
+            <h1 className="adultsOnlyCountryTitle">{currentCountry}</h1>
             <div className="fbAlbumsAdultsOnly">
-                {loadAlbums(cancunLinks)}
+                {loadAlbums(handleArrayPassing(currentCountry))}
             </div>
         </div>
     )
