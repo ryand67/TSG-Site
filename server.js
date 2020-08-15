d4129af876688b6f8ff3e02b7fee8a473645a14c
ryand67 <rwdevenney@gmail.com>
2020-08-15 15:36:25 -0400

    server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const cors = require('cors');
@@ -10,9 +11,11 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
