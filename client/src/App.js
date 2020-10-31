e9fd909dbe3e60ff56f41b140a89667bacff57dc
ryand67 <rwdevenney@gmail.com>
2020-10-30 21:18:22 -0400

    fixes

--- client/src/App.js
+++ client/src/App.js
@@ -69,16 +69,19 @@ function App() {
         <Route exact path="/meetings">
           <Hero heroHandleContactClose={contactCloseCallback} heroHandleContact={contactCallback} title="Best Meeting Ever" desc="We specialize in handling all of the specifics for your meeting or incentive group.  We handle everything from helping you find the perfect destination and resort to planning the events and menus with our contacts on property.  We can even accompany your group to oversee all of the those little details so that your employees can enjoy the whole event instead of worrying about following up on all those particulars. Reach out to us to get started planning your event Info@TravelSalesGroup.com or 888-382-8847, option 1." pictures={realHeroPics} hrVisible={true} />
           <Meetings />
+          <Footer />
         </Route>
 
         <Route exact path="/rolling-on-the-river">
           <Hero heroHandleContactClose={contactCloseCallback} heroHandleContact={contactCallback} title="Rolling on the River" pictures={realHeroPics} hrVisible={false} />
           <River />
+          <Footer />
         </Route>
 
         <Route exact path="/team">
           <Hero heroHandleContactClose={contactCloseCallback} heroHandleContact={contactCallback} title="Meet Our Team" desc="Meet our team of agents ready to send you on the vacation of your dreams." pictures={realHeroPics} hrVisible={true} />
           <TeamPage />
+          <Footer />
         </Route>
       </div>
     </Router>
