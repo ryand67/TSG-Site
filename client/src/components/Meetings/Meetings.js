894131c7a4cfb8439149b8f9fe067d4ff0f9f063
ryand67 <rwdevenney@gmail.com>
2020-12-03 23:53:15 -0500

    meeting pictures

--- client/src/components/Meetings/Meetings.js
+++ client/src/components/Meetings/Meetings.js
@@ -5,10 +5,16 @@ import pic2 from '../../Assets/MeetingPics/2.jpg';
 import pic3 from '../../Assets/MeetingPics/3.jpg';
 import pic4 from '../../Assets/MeetingPics/4.jpg';
 import pic5 from '../../Assets/MeetingPics/5.jpg';
+import pic6 from '../../Assets/MeetingPics/6.jpg';
+import pic7 from '../../Assets/MeetingPics/7.jpg';
+import pic8 from '../../Assets/MeetingPics/8.jpg';
+import pic9 from '../../Assets/MeetingPics/9.jpg';
+import pic10 from '../../Assets/MeetingPics/10.jpg';
+import pic11 from '../../Assets/MeetingPics/11.jpg';
 
 export default () => {
 
-    const meetingsPics = [['http://23.21.66.147/mediasite/wp-content/uploads/2014/09/SEARM_Ballroom_School_1A.jpg', 'http://23.21.66.147/mediasite/wp-content/uploads/2014/09/SEARM_Fuego_Breakout_Room_2-458x305.jpg'], ['http://23.21.66.147/mediasite/wp-content/uploads/2014/09/SEARM_GalaDinner_Ceiba_1-458x305.jpg', 'http://23.21.66.147/mediasite/wp-content/uploads/2014/09/SEARM_MICE_GalaDinner_pool_2A.jpg'], ['http://23.21.66.147/mediasite/wp-content/uploads/2020/02/NOSRC-GRP-BALLROOM-3A-458x305.jpg', 'http://23.21.66.147/mediasite/wp-content/uploads/2018/01/DREVC-WED-GalaDinner-Pier-2A-RGB-458x305.jpg'], ['http://23.21.66.147/mediasite/wp-content/uploads/2018/01/DREVC-GRP-Ballroom-1A-RGB-458x305.jpg', 'http://23.21.66.147/mediasite/wp-content/uploads/2018/01/DREVC-GRP-Breakout-1B-RGB-458x305.jpg'], ['http://23.21.66.147/mediasite/wp-content/uploads/2018/01/DREVC-GRP-PoolTerrace-2A-RGB-1024x682.jpg', pic1], [pic2, pic3], [pic4, pic5]]
+    const meetingsPics = [['http://23.21.66.147/mediasite/wp-content/uploads/2014/09/SEARM_Ballroom_School_1A.jpg', 'http://23.21.66.147/mediasite/wp-content/uploads/2014/09/SEARM_Fuego_Breakout_Room_2-458x305.jpg'], ['http://23.21.66.147/mediasite/wp-content/uploads/2014/09/SEARM_GalaDinner_Ceiba_1-458x305.jpg', 'http://23.21.66.147/mediasite/wp-content/uploads/2014/09/SEARM_MICE_GalaDinner_pool_2A.jpg'], ['http://23.21.66.147/mediasite/wp-content/uploads/2020/02/NOSRC-GRP-BALLROOM-3A-458x305.jpg', 'http://23.21.66.147/mediasite/wp-content/uploads/2018/01/DREVC-WED-GalaDinner-Pier-2A-RGB-458x305.jpg'], ['http://23.21.66.147/mediasite/wp-content/uploads/2018/01/DREVC-GRP-Ballroom-1A-RGB-458x305.jpg', 'http://23.21.66.147/mediasite/wp-content/uploads/2018/01/DREVC-GRP-Breakout-1B-RGB-458x305.jpg'], ['http://23.21.66.147/mediasite/wp-content/uploads/2018/01/DREVC-GRP-PoolTerrace-2A-RGB-1024x682.jpg', pic1], [pic2, pic3], [pic4, pic7], [pic8, pic9], [pic10, pic11], [pic5, pic6]]
 
     return (
         <div className="meetingsDiv">
