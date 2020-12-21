9cbe5f856479666cfa4054d01e95381f368b8d8a
ryand67 <rwdevenney@gmail.com>
2020-12-21 14:55:51 -0500

    url

--- client/src/App.js
+++ client/src/App.js
@@ -60,7 +60,7 @@ function App() {
         {isMobileForNav ? <MobileNav openMobileMenuCB={openMobileMenuCB} handleClose={closeMobileMenuCB} handleNavContact={contactCallback} /> : <Nav handleNavContact={contactCallback} />}
         {stateNavContact ? <Contact handleNavContactClose={contactCloseCallback} /> : ''}
         {openMobileMenu && isMobileForNav ? <MobileNavModal handleNavContact={contactCallback} closeMenu={closeMobileMenuCB} /> : ''}
-        <Route exact path={"/" || "/home.html"}>
+        <Route exact path="/">
           <Hero title="TravelSalesGroup" desc="“And the purpose of life, after all, is to live it, to taste experience to the utmost, to reach out eagerly and without fear for newer and richer experience.” — Eleanor Roosevelt" hrVisible={true} pictures={realHeroPics} />
           <ServiceCardSection />
           <OurTeamSection />
