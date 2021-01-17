ec3ae1760d34d3960fb213e1208e56140896214f
ryand67 <rwdevenney@gmail.com>
2021-01-16 19:12:51 -0500

    lazy loading

--- client/src/components/PickThePlace/PickThePlace.js
+++ client/src/components/PickThePlace/PickThePlace.js
@@ -54,7 +54,7 @@ function PickThePlace() {
             return (
             <>
                 <h1 className="fbHeader">{item[1]}</h1>
-                <iframe key={i} src={item[0]} width={window.innerWidth > 575 ? "552" : "325"} height="792" style={iframeStyle} scrolling="no" frameBorder="0"  allow="encrypted-media"></iframe>
+                <iframe loading="lazy" key={i} src={item[0]} width={window.innerWidth > 575 ? "552" : "325"} height="792" style={iframeStyle} scrolling="no" frameBorder="0"  allow="encrypted-media"></iframe>
             </>
             )
         })
