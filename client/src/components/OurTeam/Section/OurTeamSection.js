81ea7d23eff75564ce9e95c5ae3caef614928db4
Ryan Devenney <rwdevenney@gmail.com>
2022-03-14 20:16:22 -0400

    bridget

--- client/src/components/OurTeam/Section/OurTeamSection.js
+++ client/src/components/OurTeam/Section/OurTeamSection.js
@@ -6,66 +6,89 @@ import FrankPic from '../../../Assets/ProfilePhotos/Frank.jpg';
 import coralPic from '../../../Assets/ProfilePhotos/Coral.jpeg';
 import heatherPic from '../../../Assets/ProfilePhotos/Heather.jpg';
 import tanyaPic from '../../../Assets/ProfilePhotos/TanyaNew.png';
-import jennPic from '../../../Assets/ProfilePhotos/Jenn.jpg'
+import jennPic from '../../../Assets/ProfilePhotos/Jenn.jpg';
+import bridgetPic from '../../../Assets/ProfilePhotos/Bridget.jpg';
 
 const OurTeamSection = () => {
-    const agents = [{
-        name: 'Heather DiPietro',
-        picture: heatherPic,
-        desc: 'Heather is an expert with your Luxury Experiences, Honeymoons, Anniversaries, Destination Weddings, Incentive and Conference Groups, Europe, River Cruises, Caribbean, Mexico, Tahiti, Bora Bora, New Zealand, Hawaii.',
-        phone: '518-885-8035',
-        email: 'Heather@TravelSalesGirl.com'
-    },{
-        name: 'Jenn McDonald',
-        picture: jennPic,
-        desc: 'Jenn expertly plans Family Travel in Mexico and the Caribbean, Beach Honeymoons, Hawaii.',
-        phone: '215-491-4308',
-        email: 'Jenn@TravelSalesGal.com'
-    }, {
-        name: '​Tanya Nghiem',
-        picture: tanyaPic,
-        desc: 'Our resident Disney Princess (including Disney cruises, Adventures by Disney, and all parks), Universal Vacations, Family Travel, Hawaii, Mexico.',
-        phone: '267-483-5767',
-        email: '​Tanya@TravelSalesGroup.com'
-    },{
-        name: 'Coral Staton',
-        picture: coralPic,
-        desc: 'Namaste – Yoga Retreats, Adventure Travel, Thailand, South America, Family Travel, Some Cruises.',
-        phone: '770-272-5397​',
-        email: 'Coral@TravelSalesGroup.com'
-    },{
-        name: 'Frank DiPietro',
-        picture: FrankPic,
-        desc: 'Specialties include AMAWaterways River Cruises, Viking River Cruises, Golf and Fishing Trips, European Adventures.',
-        phone: '518-369-5730',
-        email: 'Frank@TravelSalesGuy.com'
-    }]
+	const agents = [
+		{
+			name: 'Heather DiPietro',
+			picture: heatherPic,
+			desc: 'Heather is an expert with your Luxury Experiences, Honeymoons, Anniversaries, Destination Weddings, Incentive and Conference Groups, Europe, River Cruises, Caribbean, Mexico, Tahiti, Bora Bora, New Zealand, Hawaii.',
+			phone: '518-885-8035',
+			email: 'Heather@TravelSalesGirl.com',
+		},
+		{
+			name: 'Jenn McDonald',
+			picture: jennPic,
+			desc: 'Jenn expertly plans Family Travel in Mexico and the Caribbean, Beach Honeymoons, Hawaii.',
+			phone: '215-491-4308',
+			email: 'Jenn@TravelSalesGal.com',
+		},
+		{
+			name: '​Tanya Nghiem',
+			picture: tanyaPic,
+			desc: 'Our resident Disney Princess (including Disney cruises, Adventures by Disney, and all parks), Universal Vacations, Family Travel, Hawaii, Mexico.',
+			phone: '267-483-5767',
+			email: '​Tanya@TravelSalesGroup.com',
+		},
+		{
+			name: 'Coral Staton',
+			picture: coralPic,
+			desc: 'Namaste – Yoga Retreats, Adventure Travel, Thailand, South America, Family Travel, Some Cruises.',
+			phone: '770-272-5397​',
+			email: 'Coral@TravelSalesGroup.com',
+		},
+		{
+			name: 'Frank DiPietro',
+			picture: FrankPic,
+			desc: 'Specialties include AMAWaterways River Cruises, Viking River Cruises, Golf and Fishing Trips, European Adventures.',
+			phone: '518-369-5730',
+			email: 'Frank@TravelSalesGuy.com',
+		},
+		{
+			name: 'Bridget Merritt',
+			picture: bridgetPic,
+			desc: 'She loves working with families, small groups and booking romantic vacations for couples and specializes in Mexico, Punta Cana and the Caribbean.',
+			phone: '704-437-9117',
+			email: 'Bridget@TravelSalesGroup.com',
+		},
+	];
 
-    const makeCard = (item, i) => {
-        return <NewTeamCard key={i} name={item.name} picture={item.picture} desc={item.desc} phone={item.phone} email={item.email} />
-    }
+	const makeCard = (item, i) => {
+		return (
+			<NewTeamCard
+				key={i}
+				name={item.name}
+				picture={item.picture}
+				desc={item.desc}
+				phone={item.phone}
+				email={item.email}
+			/>
+		);
+	};
 
-    return(
-        <div className="ourTeamSection" id='ourTeamSection'>
-            <h2 className="teamSectionHeader">Our Agents</h2>
-            <hr className="teamSectionHr"/>
-            {/* <newTeamCard /> */}
-            <div className="cardContainer">
-                {agents.map((item, i) => {
-                    if(i < 3) {
-                        return makeCard(item, i);
-                    }
-                })}
-            </div>
-            <div className="cardContainer">
-            {agents.map((item, i) => {
-                    if(i > 2) {
-                        return makeCard(item, i);
-                    }
-                })}
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="ourTeamSection" id="ourTeamSection">
+			<h2 className="teamSectionHeader">Our Agents</h2>
+			<hr className="teamSectionHr" />
+			{/* <newTeamCard /> */}
+			<div className="cardContainer">
+				{agents.map((item, i) => {
+					if (i < 3) {
+						return makeCard(item, i);
+					}
+				})}
+			</div>
+			<div className="cardContainer">
+				{agents.map((item, i) => {
+					if (i >= 3) {
+						return makeCard(item, i);
+					}
+				})}
+			</div>
+		</div>
+	);
+};
 
 export default OurTeamSection;
