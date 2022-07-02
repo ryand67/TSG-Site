aba55e8f2720d73cbaadd3fb8518eabf736bdd76
Ryan Devenney <rwdevenney@gmail.com>
2022-07-02 12:15:42 -0400

    typo

--- client/src/components/HowWeWork/HowWeWork.js
+++ client/src/components/HowWeWork/HowWeWork.js
@@ -115,9 +115,7 @@ const HowWeWork = () => {
 			</p>
 			<p className="howWeWorkPoint">
 				3. Deposit & Final Payment: Once you've chosen your perfect itinerary,
-				you'll be given deposit and final payment information. *Please remember
-				that the plan to book deposit that you have paid is applied at final
-				payment or is non refundable.
+				you'll be given deposit and final payment information.
 			</p>
 			<p className="howWeWorkPoint">
 				4. Documents: You will receive your final documents within 1-2 weeks of
